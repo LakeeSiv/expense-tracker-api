58010eb4b6b140126ff2cd485805c8d2da5c3788
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-12-26 15:06:17 +0000

    returning hash

--- src/resolvers/UtilResolver.ts
+++ src/resolvers/UtilResolver.ts
@@ -1,9 +1,10 @@
+import { hash } from "../utils/hash";
 import { Arg, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class UtilResolver {
   @Query(() => String)
   getHash(@Arg("input", () => String) input: string) {
-    return input;
+    return hash(input);
   }
 }
